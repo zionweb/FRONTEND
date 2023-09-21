9dc787e71177b033b4c12a1cb46d44effce40750
guanabana <jemarouy@gmail.com>
2023-09-21 18:40:33 -0300

    Agregué componente footer a la app

--- ingresosygastosfrontend/src/App.js
+++ ingresosygastosfrontend/src/App.js
@@ -1,11 +1,12 @@
 import Body from "./componentes/Body";
 import "./estilos.css";
-
+import Footer from "./componentes/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ ingresosygastosfrontend/src/componentes/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="sticky-footer bg-white">
+    <div className="container my-auto">
+        <div className="copyright text-center my-auto">
+            <span>Copyright &copy; Gastos</span>
+        </div>
+    </div>
+</footer>
+  )
+}
+
+export default Footer
